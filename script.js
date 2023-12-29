d0f01a21882606906e5c30369a443ea3d1bee2ed
sofffja <sofi.efron@gmail.com>
2023-12-29 10:49:09 -0300

    Update operations functions

--- script.js
+++ script.js
@@ -127,18 +127,7 @@ function operate(a, operator, b) {
   }
 }
 
-function add(a, b) {
-  return a + b
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
